9b47a429baca1d6fd8a51ba2cceb63f9764de7f6
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 14:20:47 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const getSumBtn = document.createElement("button");
+const table = document.querySelector("table");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let tr = document.createElement("tr");
+tr.setAttribute("id","ans");
 const getSum = () => {
-//Add your code here
-  
+	tr.innerText = 410 + " " + 333;
 };
-
+table.appendChild(tr);
 getSumBtn.addEventListener("click", getSum);
-
